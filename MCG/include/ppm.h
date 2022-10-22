1308680505bb3c230db8ba12eeb6b45e512f1b5d
Kenan Marasli <34400938+kenanmarasli@users.noreply.github.com>
2022-10-22 20:12:34 +0300

    Add source files

---
+++ MCG/include/ppm.h
@@ -0,0 +1,8 @@
+#ifndef METUCENGGRAPH_PPM
+#define METUCENGGRAPH_PPM
+
+namespace MetuCengGraph {
+void write_ppm(const char *filename, unsigned char *data, int width,
+               int height);
+}
+#endif
